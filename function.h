9d31de0d411239d77ac36c0ed6ee8b633aac4d90
AmineHamzaoui <aminenss99@gmail.com>
2022-11-07 09:49:09 +0100

    update

--- function.h
+++ function.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+#include <stdio.h>
 typedef struct 
 { int jour;
 int mois;
@@ -11,5 +14,8 @@ char orientation[10];
 char profession[30];
 date date_ajout;
 int age; } LE;
+int Modifier_LE(char* LE, int id, LE L1);
+int Supprimer_LE(char* LE, int id);
 
-
+#endif //HEADER_H_INCLUDED
+	
